9ddb4ae044e91aea892f116b1201263dd75fff42
Noam Pdut <noampdut@gmail.com>
2022-06-13 22:09:25 +0300

    fix chat screen

--- app/src/main/java/com/example/myapplication/ChatActivity.java
+++ app/src/main/java/com/example/myapplication/ChatActivity.java
@@ -2,9 +2,8 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
     private AppDb db;
     private MessagesDao messagesDao;
     private ArrayList<com.example.myapplication.Message> messages = new ArrayList<>();
-    private ArrayAdapter<com.example.myapplication.Message> adapter;
+    private MessagesAdapter adapter;
     private RecyclerView rcMessages;
     private TextView tvContact;
 
@@ -40,10 +38,20 @@ public class ChatActivity extends AppCompatActivity {
                 .allowMainThreadQueries().build();
         messagesDao = db.messagesDao();
 
-        MessagesAdapter adapter = new MessagesAdapter(this, messages);
+        //MessagesAdapter adapter = new MessagesAdapter(this, messages);
+        adapter = new MessagesAdapter(this, messages);
         rcMessages = findViewById(R.id.recycler_view);
         rcMessages.setLayoutManager(new LinearLayoutManager(this));
         rcMessages.setAdapter(adapter);
+
+        Button btnSaveNewMessage = findViewById(R.id.btnSendMessage);
+        btnSaveNewMessage.setOnClickListener(v -> {
+            EditText newMessage = findViewById(R.id.message_box);
+            Message message = new Message(newMessage.getText().toString(), "11:45", true,"ni");
+            messagesDao.insert(message);
+            finish();
+        });
+
 
         /*FloatingActionButton btnAdd = findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(view ->{
@@ -56,6 +64,23 @@ public class ChatActivity extends AppCompatActivity {
         //lvMessages = findViewById(R.id.lvMessages); ///????
         //lvMessages.setAdapter(adapter);
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        messages.clear();
+        messages.addAll(messagesDao.get("ni"));
+        //recyclerView.setLayoutManager((new LinearLayoutManager(this)));
+        //recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        //contactAdapter = new ContactAdapter(contacts);
+        rcMessages.setAdapter(adapter);
+        //contacts = contactDao.index();
+        //contactAdapter.notifyDataSetChanged();
+
+        adapter.notifyDataSetChanged();
+    }
+
 /*    @Override
     protected void onResume(){
         super.onResume();
